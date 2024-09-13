f2d893803e221920d51401cf6f7071faec172f3d
Qha <jeyzhen812@gmail.com>
2024-09-13 13:50:17 +0000

    Upload Files

---
+++ recharge/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="./favicon.ico"><title>mtrweb</title><script defer="defer" src="./js/chunk-vendors.96634890.js" charset="utf-8"></script><script defer="defer" src="./js/app.bd73ffeb.js" charset="utf-8"></script><link href="./css/app.ab9b5db7.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but mtrweb doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+        </noscript><div id="app">
+
+        </div>
+        <center><a style="color: #fff;text-decoration: none;" class='icp' href="https://beian.miit.gov.cn/" target="_blank">津ICP备2024020589号-1</a></center>
+    </body>
+    </html>
